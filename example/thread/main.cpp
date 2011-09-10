6d43b2e6749fb363eaf74cf0a57cf3d35be1ae54
Evgeny Proydakov <lord.tiran@gmail.com>
2011-09-10 11:18:24 +0400

    Added thead example

---
+++ example/thread/main.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Copyright (c) 2011 Evgeny Proydakov <lord.tiran@gmail.com>
+ */
+
+#include <iostream>
+#include <boost/thread.hpp>
+
+int main( int argc, char *argv[] )
+{
+    (void)argc;
+    (void)argv;
+
+    return 1;
+}
