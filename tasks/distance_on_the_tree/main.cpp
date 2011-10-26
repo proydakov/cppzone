3da55d986ec662023c44862d98e3cbee8908245c
Evgeny Proydakov <lord.tiran@gmail.com>
2011-10-26 21:41:33 +0400

    Updated distance_on_the_tree :
    1) added print method
    2) added new test

--- tasks/distance_on_the_tree/main.cpp
+++ tasks/distance_on_the_tree/main.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <algorithm>
 
-//#define TEST // DEFINE FOR BUILD TEST EXAMPLE
+#define TEST // DEFINE FOR BUILD TEST EXAMPLE
 
 typedef long long length_type;
 typedef int index_type;
@@ -17,28 +17,56 @@ class node
 {
 public:
 	node() :
-	    parent(NULL), 
-	    length_to_the_parent(0), 
-	    parent_name(0)
+	    m_parent(NULL), 
+	    m_length_to_the_parent(0), 
+	    m_parent_name(0)
 	{
 	}
 	
 	bool is_head() const {
-		return parent == NULL;
+		return m_parent == NULL;
 	}
 	
 	friend bool operator==(const node& a_node, const node& b_node) {
-		return a_node.parent == b_node.parent &&
-				a_node.length_to_the_parent == b_node.length_to_the_parent &&
-				a_node.parent_name == b_node.parent_name;
+		return a_node.m_parent == b_node.m_parent &&
+				a_node.m_length_to_the_parent == b_node.m_length_to_the_parent &&
+				a_node.m_parent_name == b_node.m_parent_name;
 	}
 	
-	node* parent;
-	index_type length_to_the_parent;
-	index_type parent_name;
+	node* m_parent;
+	index_type m_length_to_the_parent;
+	index_type m_parent_name;
+	
+	node(const node& src);
+	node& operator=(const node& src);
+	
+private:
+	void copy_from(const node& src);
 };
 typedef std::pair<index_type, node> graph_element;
 typedef std::map<index_type, node> graph;
+
+node::node(const node& src)
+{
+	copy_from(src);
+}
+
+node& node::operator=(const node& src)
+{
+	if(this == &src) {
+		return (*this);
+	}
+	copy_from(src);
+	
+	return (*this);
+}
+
+void node::copy_from(const node& src)
+{
+	m_parent = src.m_parent;
+	m_length_to_the_parent = src.m_length_to_the_parent;
+	m_parent_name = src.m_parent_name;
+}
 
 typedef std::pair<index_type, length_type> way_element;
 typedef std::map<index_type, length_type> way;
@@ -51,8 +79,13 @@ void read_input_task(task& data);
 
 length_type search_distances_on_the_graph(const graph& data, const task_pair& pair);
 
-void create_example_graph(graph& data);
-void create_example_task(task& data);
+void create_example_graph_1(graph& data);
+void create_example_graph_2(graph& data);
+void create_example_task_1(task& data);
+void create_example_task_2(task& data);
+
+void print(graph& data);
+void print(task& data);
 
 int main(int argc, char *argv[])
 {
@@ -63,16 +96,21 @@ int main(int argc, char *argv[])
 #ifndef TEST
     read_input_data(my_graph);
 #else
-    create_example_graph(my_graph);
+    create_example_graph_2(my_graph);
 #endif
-    
+	
     task my_task;
 #ifndef TEST
     read_input_task(my_task);
 #else
-    create_example_task(my_task);
+    create_example_task_2(my_task);
 #endif
     
+#ifdef COMMENT
+	print(my_graph);
+	print(my_task);
+#endif
+	
     task::const_iterator endIt = my_task.end();
     for(task::const_iterator it = my_task.begin(); it != endIt; ++it) {
 #ifdef TEST
@@ -108,9 +146,9 @@ void read_input_data(graph& data)
 		std::cin >> graph_node_name;
 		std::cin >> length_to_the_parent;
 		
-		my_node.length_to_the_parent = length_to_the_parent;
-		my_node.parent_name = graph_parent_name;
-		my_node.parent = &data.find(graph_parent_name)->second;
+		my_node.m_length_to_the_parent = length_to_the_parent;
+		my_node.m_parent_name = graph_parent_name;
+		my_node.m_parent = &data.find(graph_parent_name)->second;
 		
 		data.insert(graph_element(graph_node_name, my_node));
 	}	
@@ -157,9 +195,9 @@ length_type search_distances_on_the_graph(const graph& data, const task_pair& pa
 	const node* element = &it->second;
 	
 	while(!element->is_head()) {
-		length += element->length_to_the_parent;
-		my_way.insert(way_element(element->parent_name, length));
-		element = element->parent;
+		length += element->m_length_to_the_parent;
+		my_way.insert(way_element(element->m_parent_name, length));
+		element = element->m_parent;
 	}
 	
 	length = 0;
@@ -172,16 +210,16 @@ length_type search_distances_on_the_graph(const graph& data, const task_pair& pa
 	element = &it->second;
 	
 	while(my_way.insert(way_element(next_element, length)).second) {
-		length += element->length_to_the_parent;
-		next_element = element->parent_name;
-		element = element->parent;
+		length += element->m_length_to_the_parent;
+		next_element = element->m_parent_name;
+		element = element->m_parent;
 	}
 	length_type main_len = my_way.find(next_element)->second + length;
 	
 	return main_len;
 }
 
-void create_example_graph(graph& data)
+void create_example_graph_1(graph& data)
 {
     node my_node;
     index_type graph_parent_name;
@@ -193,45 +231,81 @@ void create_example_graph(graph& data)
     // node 2
     graph_node_name = 2;
     graph_parent_name = 1;
-    my_node.length_to_the_parent = 7;
-    my_node.parent = &data.find(graph_parent_name)->second;
-    my_node.parent_name = graph_parent_name;
+    my_node.m_length_to_the_parent = 7;
+    my_node.m_parent = &data.find(graph_parent_name)->second;
+    my_node.m_parent_name = graph_parent_name;
     data.insert(graph_element(graph_node_name, my_node));
     
     // node 3
     graph_node_name = 3;
     graph_parent_name = 2;
-    my_node.length_to_the_parent = 3;
-    my_node.parent = &data.find(graph_parent_name)->second;
-    my_node.parent_name = graph_parent_name;
+    my_node.m_length_to_the_parent = 3;
+    my_node.m_parent = &data.find(graph_parent_name)->second;
+    my_node.m_parent_name = graph_parent_name;
     data.insert(graph_element(graph_node_name, my_node));
     
     // node 4
     graph_node_name = 4;
     graph_parent_name = 2;
-    my_node.length_to_the_parent = 6;
-    my_node.parent = &data.find(graph_parent_name)->second;
-    my_node.parent_name = graph_parent_name;
+    my_node.m_length_to_the_parent = 6;
+    my_node.m_parent = &data.find(graph_parent_name)->second;
+    my_node.m_parent_name = graph_parent_name;
     data.insert(graph_element(graph_node_name, my_node));
     
     // node 5
     graph_node_name = 5;
     graph_parent_name = 4;
-    my_node.length_to_the_parent = 3;
-    my_node.parent = &data.find(graph_parent_name)->second;
-    my_node.parent_name = graph_parent_name;
+    my_node.m_length_to_the_parent = 3;
+    my_node.m_parent = &data.find(graph_parent_name)->second;
+    my_node.m_parent_name = graph_parent_name;
     data.insert(graph_element(graph_node_name, my_node));
     
     // node 6
     graph_node_name = 6;
     graph_parent_name = 5;
-    my_node.length_to_the_parent = 1;
-    my_node.parent = &data.find(graph_parent_name)->second;
-    my_node.parent_name = graph_parent_name;
+    my_node.m_length_to_the_parent = 1;
+    my_node.m_parent = &data.find(graph_parent_name)->second;
+    my_node.m_parent_name = graph_parent_name;
     data.insert(graph_element(graph_node_name, my_node));
 }
 
-void create_example_task(task& data)
+void create_example_graph_2(graph& data)
+{
+    node my_node;
+    index_type graph_parent_name;
+    
+    //data.resize(4);
+    
+    // node 1
+    index_type graph_node_name = 1;
+    data[0] = my_node;
+    
+    // node 2
+    graph_node_name = 3;
+    graph_parent_name = 1;
+    my_node.m_length_to_the_parent = 0;
+    my_node.m_parent = &data[graph_parent_name - 1];
+    my_node.m_parent_name = graph_parent_name;
+    data[graph_node_name - 1] = my_node;
+    
+    // node 3
+    graph_node_name = 2;
+    graph_parent_name = 3;
+    my_node.m_length_to_the_parent = 3;
+    my_node.m_parent = &data[graph_parent_name - 1];
+    my_node.m_parent_name = graph_parent_name;
+    data[graph_node_name - 1] = my_node;
+    
+    // node 4
+    graph_node_name = 4;
+    graph_parent_name = 3;
+    my_node.m_length_to_the_parent = 2;
+    my_node.m_parent = &data[graph_parent_name - 1];
+    my_node.m_parent_name = graph_parent_name;
+    data[graph_node_name - 1] = my_node;
+}
+
+void create_example_task_1(task& data)
 {
     data.push_back(task_pair(1, 6));
     data.push_back(task_pair(1, 3));
@@ -240,4 +314,31 @@ void create_example_task(task& data)
     data.push_back(task_pair(4, 3));
     data.push_back(task_pair(1, 2));
     data.push_back(task_pair(1, 3));
+}
+
+void create_example_task_2(task& data)
+{
+    data.push_back(task_pair(1, 2));
+    data.push_back(task_pair(2, 1));
+}
+
+void print(graph& data)
+{
+	int size = data.size();
+	std::cout << "GRAPH\n" << "size:  " << size << std::endl;
+	for(int i = 0; i < size; ++i) {
+		std::cout << data[i].m_parent_name << " " << i + 1 << " "
+		          << data[i].m_length_to_the_parent << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void print(task& data)
+{
+	std::cout << "TASK" << std::endl;
+	task::const_iterator endIt = data.end();
+	for(task::const_iterator it = data.begin(); it != endIt; ++it) {
+		std::cout << it->first << " " << it->second << std::endl;
+	}
+	std::cout << std::endl;
 }
