8d018b71ef67b9c98bd4ff745a549dffc646e1f5
Evgeny Proydakov <lord.tiran@gmail.com>
2011-10-28 14:09:52 +0400

    Updated distination on the tree task : completed task

--- tasks/distance_on_the_tree/main.cpp
+++ tasks/distance_on_the_tree/main.cpp
@@ -6,361 +6,186 @@
 #include <list>
 #include <vector>
 #include <iostream>
-#include <algorithm>
+#include <assert.h>
 
-//#define TEST // DEFINE FOR BUILD TEST EXAMPLE
-//#define COMMENT
+//#define COMMENT_OUTPUT
+//#define WAIT_EXIT
 
-typedef long long length_type;
-typedef int index_type;
+typedef unsigned int uint;
+typedef long long distance;
 
-class node
+class tree
 {
 public:
-	node() :
-	    m_parent(NULL), 
-	    m_length_to_the_parent(-1), 
-	    m_parent_name(0)
-	{
-	}
-	
-	bool is_head() const {
-		return m_parent == NULL;
-	}
-	
-	bool is_valid() {
-		return m_length_to_the_parent != -1;
-	}
-	
-	friend bool operator==(const node& a_node, const node& b_node) {
-		return a_node.m_parent == b_node.m_parent &&
-				a_node.m_length_to_the_parent == b_node.m_length_to_the_parent &&
-				a_node.m_parent_name == b_node.m_parent_name;
-	}
-	
-	node* m_parent;
-	index_type m_length_to_the_parent;
-	index_type m_parent_name;
-	
-	node(const node& src);
-	node& operator=(const node& src);
-	
+        tree(uint size);
+
+        bool insert_arc(uint child_id, uint parent_id, uint weight);
+        distance calculate_distance(uint first_node_id, uint second_node_id);
+
 private:
-	void copy_from(const node& src);
+        struct node;
+        typedef std::vector<node> container;
+
+        typedef std::pair<uint, distance> way_element;
+        typedef std::map<uint, distance> way;
+
+        container m_tree;
 };
-typedef std::vector<node> graph;
 
-node::node(const node& src)
-{
-	copy_from(src);
-}
-
-node& node::operator=(const node& src)
-{
-	if(this == &src) {
-		return (*this);
-	}
-	copy_from(src);
-	
-	return (*this);
-}
-
-void node::copy_from(const node& src)
-{
-	m_parent = src.m_parent;
-	m_length_to_the_parent = src.m_length_to_the_parent;
-	m_parent_name = src.m_parent_name;
-}
-
-typedef std::pair<index_type, length_type> way_element;
-typedef std::map<index_type, length_type> way;
-
-typedef std::pair<index_type, index_type> task_pair;
+typedef std::pair<uint, uint> task_pair;
 typedef std::list<task_pair> task;
 
-void read_input_data(graph& data);
 void read_input_task(task& data);
 
-length_type search_distances_on_the_graph(const graph& data, const task_pair& pair);
-
-void create_example_graph_1(graph& data);
-void create_example_graph_2(graph& data);
-
-void create_example_task_1(task& data);
-void create_example_task_2(task& data);
-
-void print(graph& data);
-void print(task& data);
-
-int main(int argc, char *argv[])
+int main()
 {
-    (void)argc;
-    (void)argv;
-    
-    graph my_graph;
-#ifndef TEST
-    read_input_data(my_graph);
-#else
-    create_example_graph_2(my_graph);
+        uint data_size = 0;
+#ifdef COMMENT_OUTPUT
+        std::cout << "Enter number of graph arc: ";
 #endif
-	
-    task my_task;
-#ifndef TEST
-    read_input_task(my_task);
-#else
-    create_example_task_2(my_task);
+        std::cin >> data_size;
+
+        tree i_tree(data_size);
+
+        uint graph_node_name;
+        uint graph_parent_name;
+        uint length_to_the_parent;
+
+        for(uint i = 1; i < data_size; ++i) {
+                std::cin >> graph_node_name;
+                std::cin >> graph_parent_name;
+                std::cin >> length_to_the_parent;
+
+                i_tree.insert_arc(graph_node_name, graph_parent_name, length_to_the_parent);
+        }
+
+        task i_task;
+        read_input_task(i_task);
+        task::const_iterator endIt = i_task.end();
+        for(task::const_iterator it = i_task.begin(); it != endIt; ++it) {
+                std::cout << i_tree.calculate_distance(it->first, it->second) << std::endl;
+        }
+#ifdef WAIT_EXIT
+        char exit;
+        std::cin >> exit;
 #endif
-    
-#ifdef COMMENT
-	print(my_graph);
-	print(my_task);
-#endif
-	
-    task::const_iterator endIt = my_task.end();
-    for(task::const_iterator it = my_task.begin(); it != endIt; ++it) {
-#ifdef COMMENT
-        std::cout << "PAIR:  " << it->first << " " << it->second << std::endl;
-        std::cout << "LENGTH:  ";
-#endif
-        std::cout << search_distances_on_the_graph(my_graph, *it) << std::endl;
-    }
-    return 0;
+        return 0;
 }
 
-void read_input_data(graph& data)
+struct tree::node
 {
-#ifdef COMMENT
-	std::cout << "Enter number of graph vertices: ";
-#endif
-	int input_size = 0;
-	std::cin >> input_size;
-	data.resize(input_size);
-	
-	index_type graph_parent_name;
-	index_type graph_node_name;
-	index_type length_to_the_parent;
-	
-	node my_node;
-	data[0] = my_node;
-	
-	//print(data);
-	
-	for(int i = 1; i < input_size; ++i) {
-#ifdef COMMENT
-		std::cout << "Enter data graph for the element " << i << " (parent, element, length_to_the_parent):  ";
-#endif
-		std::cin >> graph_parent_name;
-		std::cin >> graph_node_name;
-		std::cin >> length_to_the_parent;
-		
-		//std::cout << "graph_parent_name:  " << graph_parent_name << std::endl;
-		//std::cout << "graph_node_name:  "  << graph_node_name << std::endl;
-		//std::cout << "length_to_the_parent:  "  << length_to_the_parent << std::endl;
-		
-		if(data[graph_node_name - 1].is_valid()) {
-			std::swap(graph_parent_name, graph_node_name);
-			//std::cout << "\nSWAP\n" << std::endl;
-		}
-		
-		//std::cout << "graph_parent_name:  " << graph_parent_name << std::endl;
-		//std::cout << "graph_node_name:  "  << graph_node_name << std::endl;
-		//std::cout << "length_to_the_parent:  "  << length_to_the_parent << std::endl;
-		
-		my_node.m_length_to_the_parent = length_to_the_parent;
-		my_node.m_parent_name = graph_parent_name;
-		my_node.m_parent = &data[graph_parent_name - 1];
-		
-		data[graph_node_name - 1] = my_node;
-		
-		//print(data);
-	}	
+        node() : m_node_id(1), m_parent(parent_node(0, 0)) ,m_exist(false) {}
+        node(uint node_id) : m_node_id(node_id), m_parent(parent_node(0, 0)) ,m_exist(true) {}
+
+        bool is_head() const {
+                return m_parent.second == NULL;
+        }
+
+        bool is_exist() const {
+                return m_exist;
+        }
+
+        void enable() {
+            m_exist = true;
+        }
+        
+        typedef std::pair<uint, node*>  parent_node;
+        typedef std::list<node*>		child_nodes;
+
+        uint		m_node_id;
+        parent_node m_parent;
+        child_nodes m_childs;
+
+        bool		m_exist;
+};
+
+tree::tree(uint size)
+{
+        m_tree.resize(size);
+        if(size == 1)
+                m_tree[0].enable();
+}
+
+bool tree::insert_arc(uint child_id, uint parent_id, uint weight)
+{
+        bool exist_child = m_tree[child_id - 1].is_exist();
+        bool exist_parent = m_tree[parent_id - 1].is_exist();
+
+        node* child_ptr = !exist_child ? NULL : &m_tree[child_id - 1];
+        node* parent_ptr = !exist_parent ? NULL : &m_tree[parent_id - 1];
+
+        if(!exist_child) {
+                node child(child_id);
+                m_tree[child_id - 1] = child;
+                child_ptr = &m_tree[child_id - 1];
+        }
+        if(!exist_parent) {
+                node parent(parent_id);
+                m_tree[parent_id - 1] = parent;
+                parent_ptr = &m_tree[parent_id - 1];
+        }
+
+        child_ptr->m_parent = node::parent_node(weight, parent_ptr);
+        parent_ptr->m_childs.push_back(child_ptr);
+
+        return true;
+}
+
+distance tree::calculate_distance(uint first_node_id, uint second_node_id)
+{
+        distance length_way = 0;
+
+        uint min_element = std::min(first_node_id, second_node_id);
+        uint max_element = std::max(first_node_id, second_node_id);
+
+        if(min_element < 0 && max_element >= m_tree.size())
+                return -1;
+
+        distance length = 0;
+
+        way my_way;
+        my_way.insert(way_element(max_element, length));
+
+        node* element = &m_tree[max_element - 1];
+
+        while(!element->is_head()) {
+                length += element->m_parent.first;
+                my_way.insert(way_element(element->m_parent.second->m_node_id, length));
+                element = element->m_parent.second;
+        }
+
+        length = 0;
+        uint next_element = min_element;
+
+        element = &m_tree[min_element - 1];
+
+        while(my_way.insert(way_element(next_element, length)).second) {
+                length += element->m_parent.first;
+                next_element = element->m_parent.second->m_node_id;
+                element = element->m_parent.second;
+        }
+        length_way = my_way.find(next_element)->second + length;
+
+        return length_way;
 }
 
 void read_input_task(task& data)
 {
-#ifdef COMMENT
-	std::cout << "Enter the number of pairs to find: ";
+#ifdef COMMENT_OUTPUT
+        std::cout << "Enter the number of pairs to find: ";
 #endif
-	int size = 0;
-	std::cin >> size;
-	
-	index_type first;
-	index_type second;
-	
-	for(int i = 1; i <= size; ++i) {
-#ifdef COMMENT
-		std::cout << "Enter to find a pair " << i << " (element a, element b):  ";
+        uint size = 0;
+        std::cin >> size;
+
+        uint first;
+        uint second;
+
+        for(uint i = 0; i < size; ++i) {
+#ifdef COMMENT_OUTPUT
+                std::cout << "Enter to find a pair " << i + 1 << " (element a, element b):  ";
 #endif
-		std::cin >> first;
-		std::cin >> second;
-		data.push_back(task_pair(first, second));
-	}
-}
-
-length_type search_distances_on_the_graph(const graph& data, const task_pair& pair)
-{
-	if(data.empty())
-		return -1;
-	
-	index_type min_element = std::min(pair.first, pair.second);
-	index_type max_element = std::max(pair.first, pair.second);	
-	
-	if(min_element < 0 && max_element >= data.size())
-		return -1;
-	
-	length_type length = 0;
-	
-	way my_way;
-	my_way.insert(way_element(max_element, length));
-	
-	const node* element = &data[max_element - 1];
-	
-	while(!element->is_head()) {
-		length += element->m_length_to_the_parent;
-		my_way.insert(way_element(element->m_parent_name, length));
-		element = element->m_parent;
-	}
-	
-	length = 0;
-	index_type next_element = min_element;
-	
-	element = &data[min_element - 1];
-	
-	while(my_way.insert(way_element(next_element, length)).second) {
-		length += element->m_length_to_the_parent;
-		next_element = element->m_parent_name;
-		element = element->m_parent;
-	}
-	length_type main_len = my_way.find(next_element)->second + length;
-	
-	return main_len;
-}
-
-void create_example_graph_1(graph& data)
-{
-    node my_node;
-    index_type graph_parent_name;
-    
-    data.resize(6);
-    
-    // node 1
-    index_type graph_node_name = 1;
-    data[0] = my_node;
-    
-    // node 2
-    graph_node_name = 2;
-    graph_parent_name = 1;
-    my_node.m_length_to_the_parent = 7;
-    my_node.m_parent = &data[graph_parent_name - 1];
-    my_node.m_parent_name = graph_parent_name;
-    data[1] = my_node;
-    
-    // node 3
-    graph_node_name = 3;
-    graph_parent_name = 2;
-    my_node.m_length_to_the_parent = 3;
-    my_node.m_parent = &data[graph_parent_name - 1];
-    my_node.m_parent_name = graph_parent_name;
-    data[2] = my_node;
-    
-    // node 4
-    graph_node_name = 4;
-    graph_parent_name = 2;
-    my_node.m_length_to_the_parent = 6;
-    my_node.m_parent = &data[graph_parent_name - 1];
-    my_node.m_parent_name = graph_parent_name;
-    data[3] = my_node;
-    
-    // node 5
-    graph_node_name = 5;
-    graph_parent_name = 4;
-    my_node.m_length_to_the_parent = 3;
-    my_node.m_parent = &data[graph_parent_name - 1];
-    my_node.m_parent_name = graph_parent_name;
-    data[4] = my_node;
-    
-    // node 6
-    graph_node_name = 6;
-    graph_parent_name = 5;
-    my_node.m_length_to_the_parent = 1;
-    my_node.m_parent = &data[graph_parent_name - 1];
-    my_node.m_parent_name = graph_parent_name;
-    data[5] = my_node;
-}
-
-void create_example_graph_2(graph& data)
-{
-    node my_node;
-    index_type graph_parent_name;
-    
-    data.resize(4);
-    
-    // node 1
-    index_type graph_node_name = 1;
-    data[0] = my_node;
-    
-    // node 2
-    graph_node_name = 3;
-    graph_parent_name = 1;
-    my_node.m_length_to_the_parent = 0;
-    my_node.m_parent = &data[graph_parent_name - 1];
-    my_node.m_parent_name = graph_parent_name;
-    data[graph_node_name - 1] = my_node;
-    
-    // node 3
-    graph_node_name = 2;
-    graph_parent_name = 3;
-    my_node.m_length_to_the_parent = 3;
-    my_node.m_parent = &data[graph_parent_name - 1];
-    my_node.m_parent_name = graph_parent_name;
-    data[graph_node_name - 1] = my_node;
-    
-    // node 4
-    graph_node_name = 4;
-    graph_parent_name = 3;
-    my_node.m_length_to_the_parent = 2;
-    my_node.m_parent = &data[graph_parent_name - 1];
-    my_node.m_parent_name = graph_parent_name;
-    data[graph_node_name - 1] = my_node;
-}
-
-void create_example_task_1(task& data)
-{
-    data.push_back(task_pair(1, 6));
-    data.push_back(task_pair(1, 3));
-    data.push_back(task_pair(6, 3));
-    data.push_back(task_pair(2, 5));
-    data.push_back(task_pair(4, 3));
-    data.push_back(task_pair(1, 2));
-    data.push_back(task_pair(1, 3));
-    data.push_back(task_pair(4, 4));
-    data.push_back(task_pair(1, 1));
-    data.push_back(task_pair(6, 6));
-}
-
-void create_example_task_2(task& data)
-{
-    data.push_back(task_pair(1, 2));
-    data.push_back(task_pair(2, 1));
-}
-
-void print(graph& data)
-{
-	int size = data.size();
-	std::cout << "GRAPH\n" << "size:  " << size << std::endl;
-	for(int i = 0; i < size; ++i) {
-		std::cout << data[i].m_parent_name << " " << i + 1 << " "
-		          << data[i].m_length_to_the_parent << std::endl;
-	}
-	std::cout << std::endl;
-}
-
-void print(task& data)
-{
-	std::cout << "TASK" << std::endl;
-	task::const_iterator endIt = data.end();
-	for(task::const_iterator it = data.begin(); it != endIt; ++it) {
-		std::cout << it->first << " " << it->second << std::endl;
-	}
-	std::cout << std::endl;
+                std::cin >> first;
+                std::cin >> second;
+                data.push_back(task_pair(first, second));
+        }
 }
