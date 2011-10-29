64bc6eb103e425ac00896cec1536a4adff79878e
Evgeny Proydakov <lord.tiran@gmail.com>
2011-10-30 01:41:42 +0400

    Updated t9 task : fixed find_priority_element method in class dictionary

--- tasks/t9/main.cpp
+++ tasks/t9/main.cpp
@@ -5,78 +5,94 @@
 #include <map>
 #include <list>
 #include <iostream>
+#include <assert.h>
 
-#define TEST // DEFINE FOR BUILD TEST EXAMPLE
-#define COMMENT
+//#define COMMENT
+
+typedef unsigned int    index;
+typedef double          priority;
+typedef char            symbol;
+typedef std::string     string;
+
+const index MAX_NUM_WORDS   = 100000;
+const priority MIN_PRIORITY = (priority) 1 / MAX_NUM_WORDS;
 
 class dictionary
 {
 public:
     dictionary() :
-        m_min_delta_priority(0.00001), 
-        m_delta_priority(m_min_delta_priority),
-        m_index(0)
+        m_delta_priority(MIN_PRIORITY)
     {
     }
-    ~dictionary() {};
+    
+    bool add_word(const string& word, priority word_priority);
+    
+    void search(const string& next_symbols, string& word);
+    void next(string& word);
+    void stop_search();
     
     friend std::ostream& operator<< (std::ostream& ostr, const dictionary& book);
-    
-    bool add_word(const std::string& string, double priority);
-    bool nice(const std::string& string);
-    
-    void search(std::string& next_symbols, std::string& string);
-    void next(std::string& next_string);
-    void stop_search();
     
     void clear();
     
 private:
-    void find_priority_element(std::string& element);
+    bool nice(const string& word);
+    
+    void start_search();
+    void continue_search();
+    
+    void find_priority_word(string& word);
     
 private:
-    typedef std::pair<std::string, double> information_about_word;
-    typedef std::map<std::string, double> collection;
+    typedef std::pair<std::string, priority> information_about_word;
+    typedef std::map<std::string, priority> collection;
     typedef std::list<std::string> query;
     
     collection m_collection;
     collection m_selection;
     
     query m_query;
+    std::string m_word;
     
-    double m_min_delta_priority;
-    double m_delta_priority;
-    
-    int m_index;
+    priority m_delta_priority;
 };
 
 class phone
 {
 public:
-    phone() :
-        m_started_writing_word(false),
-        m_started_writing_mark(false)
+    phone(dictionary* phone_dictionary) :
+        m_dictionary(phone_dictionary),
+        m_state(state_end)
     {
     }
     
-    void parse_message(dictionary& i_dictionary, std::string& message);
+    void parse_message(const string& message);
     
 private:
-    void phone_keypad_transformer(char symbol, std::string& symbols);  
+    // state machine
+    enum states {
+        state_start,
+        state_enter_space,
+        state_enter_mark,
+        state_enter_word,
+        state_next_element,
+        state_end
+    };
     
 private:
-    bool m_started_writing_word;
-    bool m_started_writing_mark;
+    void phone_keypad_transformer(symbol key, string& symbols);  
+    
+    void print_mark(const string& marks, index& current_mark_index, bool& mark_enter_state);
+    void print_word(const string& word, bool& word_enter_state);
+    
+private:
+    dictionary* m_dictionary;
+    
+    states m_state;
 };
 
 void read_input_data(dictionary& data);
-void read_input_task(std::string& task);
-
-void create_example_dictionary_1(dictionary& data);
-void create_example_dictionary_2(dictionary& data);
-
-void create_example_task_1(std::string& data);
-void create_example_task_2(std::string& data);
+void read_input_task(string& task);
 
 int main(int argc, char *argv[])
 {
@@ -84,39 +100,18 @@ int main(int argc, char *argv[])
     (void)argv;
     
     dictionary i_dictionary;
-    phone i_phone;
-    std::string task;
+    phone i_phone(&i_dictionary);
+    string task;
     
-#ifdef TEST
-    // test 1
-    create_example_dictionary_1(i_dictionary);
-    std::cout << i_dictionary;
-    create_example_task_1(task);
-    std::cout << task << "\n" << std::endl;
-    
-    i_phone.parse_message(i_dictionary, task);
-    
-    i_dictionary.clear();
-    
-    // test 2
-    create_example_dictionary_2(i_dictionary);
-    std::cout << i_dictionary;
-    create_example_task_2(task);
-    std::cout << task << "\n" << std::endl;
-    
-    i_phone.parse_message(i_dictionary, task);
-    
-    i_dictionary.clear();
-#else
     read_input_data(i_dictionary);
+#ifdef COMMENT
     std::cout << i_dictionary;
-    read_input_task(task);
-    std::cout << task << "\n" << std::endl;
-    
-    i_phone.parse_message(i_dictionary, task);
-    
-    i_dictionary.clear();
 #endif
+    read_input_task(task);
+#ifdef COMMENT
+    std::cout << task << "\n" << std::endl;
+#endif
+    i_phone.parse_message(task);
     
     return 0;
 }
@@ -132,54 +127,46 @@ std::ostream& operator<< (std::ostream& ostr, const dictionary& book)
     return ostr;
 }
 
-bool dictionary::add_word(const std::string& string, double priority)
+bool dictionary::add_word(const string& word, priority word_priority)
 {
-    return m_collection.insert(information_about_word(string, priority)).second;
+    return m_collection.insert(information_about_word(word, word_priority)).second;
 }
 
-bool dictionary::nice(const std::string& string)
+bool dictionary::nice(const string& word)
 {
-    collection::iterator it = m_collection.find(string);
+    collection::iterator it = m_collection.find(word);
     bool res = it != m_collection.end();
     if(res) {
         it->second += 1 + m_delta_priority;
-        m_delta_priority += m_min_delta_priority;  
+        m_delta_priority += MIN_PRIORITY;  
     }
     return res;
 }
 
-void dictionary::search(std::string& next_symbols, std::string& string)
+void dictionary::search(const string& next_symbols, string& word)
 {
-    if(m_query.empty()) {
-        m_query.push_back(next_symbols);
-        
-        query::const_iterator it = m_query.begin();
-        for(int i = 0; i < (*it).size(); ++i) {
-            std::string symbol;
-            symbol += (*it)[i];
-            m_collection.lower_bound(symbol);
-            collection::const_iterator itEndCollection = m_collection.upper_bound(symbol);
-            for(collection::const_iterator itCollection = m_collection.begin(); itCollection != itEndCollection; ++itCollection) {
-                m_selection.insert(*itCollection);
-            }
-        }
-        ++m_index;
+    m_query.push_back(next_symbols);
+    
+    if(m_query.size() < 2) {
+        start_search();
     }
     else {
-        
+        continue_search();
     }
-    next(string);
+    find_priority_word(word);
+    m_word = word;
 }
 
-void dictionary::next(std::string& next_string)
+void dictionary::next(string& word)
 {
-    find_priority_element(next_string);
-    m_selection.erase(next_string);
+    m_selection.erase(m_word);
+    find_priority_word(word);
+    m_word = word;
 }
 
 void dictionary::stop_search()
 {
-    m_index = 0;
+    nice(m_word);
     m_query.clear();
     m_selection.clear();
 }
@@ -190,57 +177,176 @@ void dictionary::clear()
     m_collection.clear();
 }
 
-void dictionary::find_priority_element(std::string& element)
+void dictionary::start_search()
 {
-    element = m_collection.begin()->first;
-    collection::const_iterator element_iterator = m_collection.begin();
-    collection::const_iterator endIt = m_collection.end();
-    for(collection::const_iterator it = m_collection.begin(); it != endIt; ++it) {
-        if(it->second > element_iterator->second) {
+    query::const_iterator it = m_query.begin();
+    string symbols = *it;
+    
+    std::string first_symbol;
+    first_symbol += symbols[0];
+    symbol end_symbol = symbols[symbols.size() - 1];
+    
+    collection::const_iterator itCollection = m_collection.lower_bound(first_symbol);
+    collection::const_iterator endCollection = m_collection.end();
+    
+    while(itCollection != endCollection && itCollection->first[0] <= end_symbol) {
+        m_selection.insert(*itCollection);
+        ++itCollection;
+    }
+}
+
+void dictionary::continue_search()
+{
+    query::const_iterator lastIt = m_query.end();
+    --lastIt;
+    string symbols = *lastIt;
+    index verifiable_character_position = m_query.size() - 1;
+    
+    bool delete_element = false;
+    collection::iterator delete_it = m_selection.end();
+    
+    collection::const_iterator endIt = m_selection.end();
+    collection::iterator it = m_selection.begin();
+    do {
+        delete_element = true;
+        delete_it = it;
+        
+        if(it->first.size() < verifiable_character_position)
+            delete_element = false;
+        if(delete_element) {
+            for(index i = 0; i < symbols.size(); ++i) {
+                if(it->first[verifiable_character_position] == symbols[i]) {
+                    delete_element = false;
+                    break;
+                }
+            }
+        }
+        if(delete_element) {
+            m_selection.erase(delete_it);
+            delete_element = false;
+        }
+        ++it;
+    }
+    while(it != endIt);
+}
+
+void dictionary::find_priority_word(string& word)
+{
+    if(m_selection.empty()) {
+        assert(!"selection data empty");
+    }
+    index word_size = m_query.size();
+    
+    collection::const_iterator endIt = m_selection.end();
+    collection::const_iterator element_iterator = m_selection.begin();
+    
+    while(element_iterator != endIt) {
+        if(element_iterator->first.size() == word_size) {
+            word = element_iterator->first;
+            break;
+        }
+        ++element_iterator;
+    }
+    
+    for(collection::const_iterator it = element_iterator; it != endIt; ++it) {
+        if(it->second > element_iterator->second && it->first.size() == word_size) {
             element_iterator = it;
-            element = it->first;
+            word = it->first;
         }
     }
 }
 
-void phone::parse_message(dictionary& i_dictionary, std::string& message)
-{
-    char symbol;
-    for(int i = 0; i < message.size(); ++i) {
-        symbol = message[i];
-        if(symbol == ' ') {
-            if(m_started_writing_mark) {
-                m_started_writing_mark = false;
-            }
-            else if(m_started_writing_word) {
-                m_started_writing_word = false;
-            }
+void phone::parse_message(const string& message)
+{   
+    m_state = state_start;
+    
+    string marks = ".,?";
+    index current_mark_index = 0;
+    
+    string word;
+    string symbols;
+    
+    index message_size = message.size();
+    
+    symbol current_symbol;
+    
+    bool started_writing_word = false;
+    bool started_writing_mark = false;
+    
+    for(index i = 0; i < message_size; ++i) {
+        current_symbol = message[i];
+        
+        if(current_symbol == ' ') {
+            m_state = state_enter_space;
         }
-        else if(symbol == '*') {
-            if(m_started_writing_mark) {
-                
-            }
-            else if(m_started_writing_word) {
-                
-            }
+        else if(current_symbol == '*') {
+            m_state = state_next_element;
         }
-        else if(symbol == '1') {
-            if(m_started_writing_word) {
-                m_started_writing_word = false;
-            }
+        else if(current_symbol == '1') {
+            m_state = state_enter_mark;
         }
         else {
-            
+            m_state = state_enter_word;
         }
-    }    
+        
+        switch(m_state) {
+        case state_enter_space:
+            if(started_writing_word) {
+                print_word(word, started_writing_word);
+            }
+            else if(started_writing_mark) {
+                print_mark(marks, current_mark_index, started_writing_mark);
+            }
+            std::cout << ' ';
+            break;
+            
+        case state_enter_mark:
+            if(started_writing_word) {
+                print_word(word, started_writing_word);
+            }
+            else if(started_writing_mark) {
+                print_mark(marks, current_mark_index, started_writing_mark);
+            }
+            started_writing_mark = true;
+            current_mark_index = 0;
+            break;
+            
+        case state_enter_word:
+            if(started_writing_mark) {
+                print_mark(marks, current_mark_index, started_writing_mark);
+            }
+            started_writing_word = true;
+            phone_keypad_transformer(message[i], symbols);
+            m_dictionary->search(symbols, word);
+            break;
+            
+        case state_next_element:
+            if(started_writing_word) {
+                m_dictionary->next(word);
+            }
+            else if(started_writing_mark) {
+                ++current_mark_index;
+            }
+            break;
+            
+        default:
+            assert(!"unknown state");
+            break;
+        }
+    }
+    if(started_writing_word) {
+        print_word(word, started_writing_word);
+    }
+    else if(started_writing_mark) {
+        print_mark(marks, current_mark_index, started_writing_mark);
+    }
+    m_state = state_end;
+    std::cout << std::endl;
 }
 
-void phone::phone_keypad_transformer(char symbol, std::string& symbols)
+void phone::phone_keypad_transformer(symbol key, string& symbols)
 {
-    switch(symbol) {
-    case '1':
-        symbols = ".,?";
-        break;
+    switch(key) {
     case '2':
         symbols = "abc";
         break;
@@ -266,8 +372,22 @@ void phone::phone_keypad_transformer(char symbol, std::string& symbols)
         symbols = "wxyz";
         break;
     default:
+        assert(!"error input data");
         break;
     }
+}
+
+void phone::print_mark(const string& marks, index& current_mark_index, bool& mark_enter_state)
+{
+    mark_enter_state = false;
+    std::cout << marks[current_mark_index];
+}
+
+void phone::print_word(const string& word, bool& word_enter_state)
+{
+    m_dictionary->stop_search();
+    word_enter_state = false;
+    std::cout << word;
 }
 
 void read_input_data(dictionary &data)
@@ -275,23 +395,23 @@ void read_input_data(dictionary &data)
 #ifdef COMMENT
     std::cout << "Enter the size of the dictionary:  ";
 #endif // COMMENT
-    int lenght;
+    index lenght;
     std::cin >> lenght;
     
-    std::string string;
-    int priority;
+    std::string word;
+    priority word_priority;
     
-    for(int i = 1; i <= lenght; ++i) {
+    for(index i = 1; i <= lenght; ++i) {
 #ifdef COMMENT
         std::cout << "Enter data about next word (text, priority): ";
 #endif // COMMENT
-        std::cin >> string;
-        std::cin >> priority;
-        data.add_word(string, priority);
+        std::cin >> word;
+        std::cin >> word_priority;
+        data.add_word(word, word_priority);
     }
 }
 
-void read_input_task(std::string& task)
+void read_input_task(string& task)
 {
 #ifdef COMMENT
     std::cout << "Enter a sequence of characters entered by: ";
@@ -300,30 +420,4 @@ void read_input_task(std::string& task)
     std::string temp;
     std::getline(std::cin ,temp);
     task += temp;
-}
-
-void create_example_dictionary_1(dictionary& data)
-{
-    data.add_word("ad", 2);
-    data.add_word("be", 1);
-    data.add_word("not", 10);
-    data.add_word("or", 5);
-    data.add_word("to", 50);
-}
-
-void create_example_dictionary_2(dictionary& data)
-{
-    data.add_word("act", 1);
-    data.add_word("bat", 1);
-    data.add_word("cat", 1);
-}
-
-void create_example_task_1(std::string& data)
-{
-    data = "86 23* 67 668 86 231**";
-}
-
-void create_example_task_2(std::string& data)
-{
-    data = "228* 228** 228** 228**1";  
 }
