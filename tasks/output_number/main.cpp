fd9259a8dd1ecffa6e61cfdc5b20124537e857a2
Evgeny Proydakov <lord.tiran@gmail.com>
2011-11-29 00:17:04 +0400

    Updated task output number : added a new algorithm based on the data storage in a container

--- tasks/output_number/main.cpp
+++ tasks/output_number/main.cpp
@@ -2,26 +2,50 @@
  *  Copyright (c) 2011 Evgeny Proydakov <lord.tiran@gmail.com>
  */
 
+#include <list>
 #include <iostream>
 
-void recursive_output_number(int num);
+typedef int type;
+
+void recursive_output_number(type num, type base);
+void output_number(type num, type base);
 
 int main( int argc, char *argv[] )
 {
     (void)argc;
     (void)argv;
-
-    recursive_output_number(10);
+    
+    type num = 100;
+    type base = 7;
+    
+    recursive_output_number(num, base);
     std::cout << std::endl;
-
+    output_number(num, base);
+    std::cout << std::endl;
+    
     return 0;
 }
 
-void recursive_output_number(int num)
+void recursive_output_number(type num, type base)
 {
     if(num < 1)
         return;
-    recursive_output_number(num / 2);
-    std::cout << num % 2;
+    recursive_output_number(num / base, base);
+    std::cout << num % base;
 }
 
+void output_number(type num, type base)
+{
+    typedef std::list<type> container;
+    container list;
+    
+    while(num != 0) {
+        list.push_front(num % base);
+        num /= base;
+    }
+    
+    container::const_iterator endIt = list.end();
+    for(container::const_iterator it = list.begin(); it != endIt; ++it) {
+        std::cout << *it;   
+    }
+}
