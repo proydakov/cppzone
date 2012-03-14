68458e5e8b069d750c2a449e6991af7ab013d5d0
Evgeny Proydakov <lord.tiran@gmail.com>
2012-03-14 17:15:49 +0400

    Added syntax example : adress_base

---
+++ syntax/adress_base/main.cpp
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2011 Evgeny Proydakov <lord.tiran@gmail.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+    
+    int a = 'a';
+    int b = 'b';
+    
+    int& pa = a;
+    int* pb = &b;
+    
+    int a1 = --pa;
+    int b1 = pa++;
+    
+    int a2 = *(--pb);
+    int b2 = *(pb--);
+
+    std::cout << "A1 " << a1 << " B1 " << b1 << std::endl;
+    std::cout << "A2 " << a2 << " B2 " << b2 << std::endl;
+    
+    return 0;
+}
