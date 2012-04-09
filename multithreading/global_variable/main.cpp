dfbc48950b9cdbf70f3e8250dabc2d3877c2b03d
Evgeny Proydakov <lord.tiran@gmail.com>
2012-04-09 17:28:10 +0400

    Added multithreading global variable example

---
+++ multithreading/global_variable/main.cpp
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 2012 Evgeny Proydakov <lord.tiran@gmail.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+#include <iostream>
+
+#include <boost/thread.hpp>
+
+const int MAX_COUNTER_VALUE = 100000;
+
+int counter = 0;
+
+void thread_fun()
+{
+    while(true)
+        ++counter; 
+}
+
+int main( int argc, char *argv[] )
+{
+    (void)argc;
+    (void)argv;
+    
+    int thread_col = 30;
+    
+    boost::thread my_thread[thread_col];
+    for(int i = 0; i < thread_col; ++i) {
+        my_thread[0] = boost::thread(thread_fun);
+    }
+    
+    while(true) {
+        if(counter > MAX_COUNTER_VALUE)
+            break;
+    }
+    
+    std::cout << "COUNTER VALUE: " << counter << std::endl;
+    
+    return 0;
+}
