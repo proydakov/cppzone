a091b00838164ecab07f37ecd9ce02aae0704a8d
Evgeny Proydakov <lord.tiran@gmail.com>
2012-04-23 15:20:37 +0400

    Added graphics_hello_opengl example

---
+++ graphics/hello_opengl/main.cpp
@@ -0,0 +1,45 @@
+#include <gl/glut.h>
+
+#include <gl/gl.h>
+#include <gl/glu.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+
+    glBegin(GL_POLYGON);
+    glVertex3f(0.25, 0.25, 0.0);
+    glVertex3f(0.75, 0.25, 0.0);
+    glVertex3f(0.75, 0.75, 0.0);
+    glVertex3f(0.25, 0.75, 0.0);
+    glEnd();
+
+    glFlush();
+}
+
+void init()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(250, 250);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello, World!!!");
+
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+}
