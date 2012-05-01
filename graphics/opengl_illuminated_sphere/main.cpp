4f093be01e16ab7254f63fc729476f4d30e3e195
Evgeny Proydakov <lord.tiran@gmail.com>
2012-05-01 14:12:55 +0400

    Added graphics opengl illuminated sphere example

---
+++ graphics/opengl_illuminated_sphere/main.cpp
@@ -0,0 +1,113 @@
+/*
+ *  Copyright (c) 2012 Evgeny Proydakov <lord.tiran@gmail.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+const int SPHERE_PRECESSION = 50;
+
+const GLdouble OBJECT_ROTATION_DELTA = 3.0;
+static GLdouble g_objectRotationAngle = 30.0;
+
+void init()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_SMOOTH);
+
+    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat mat_shininess[] = { 50.0 };
+
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+
+    GLfloat lmodel_ambient[] = { 0.2, 0.2, 0.2, 1.0 };
+
+    GLfloat light0_position[] = { 0.0, 1.0, 0.1, 0.0 };
+    GLfloat light0_color[] = { 0.0, 1.0, 0.0, 1.0 };
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_color);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, lmodel_ambient);
+
+    GLfloat light1_position[] = { 1.0, 0.0, 0.1, 0.0 };
+    GLfloat light1_color[] = { 1.0, 0.0, 0.0, 1.0 };
+
+    glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_color);
+    glLightfv(GL_LIGHT1, GL_SPECULAR, lmodel_ambient);
+
+    GLfloat light2_position[] = { -1.0, -1.0, 0.75, 0.0 };
+    GLfloat light2_color[] = { 0.0, 0.0, 1.0, 1.0 };
+
+    glLightfv(GL_LIGHT2, GL_POSITION, light2_position);
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, light2_color);
+    glLightfv(GL_LIGHT2, GL_SPECULAR, lmodel_ambient);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_LIGHT2);
+    glEnable(GL_DEPTH_TEST);
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+
+    glPushMatrix();
+    glutSolidSphere(1.0, SPHERE_PRECESSION, SPHERE_PRECESSION);
+    //glutSolidCube(1.0);
+    glPopMatrix();
+
+    glFlush();
+}
+
+void reshape(int w, int h)
+{
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if(w <= h)
+        glOrtho(-1.5, 1.5, -1.5 * (GLdouble) h / (GLdouble) w,
+        1.5 * (GLdouble) h / (GLdouble) w, -10.0, 10.0);
+    else
+        glOrtho(-1.5 * (GLdouble) h / (GLdouble) w, 1.5 * (GLdouble) h / (GLdouble) w,
+        -1.5, 1.5, -10.0, 10.0);
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
+
+    return 0;
+}
