6a81cd221e9d4b663315caa0b3fe56b63b99898a
unknown <evgeny@evgeny-pc.(none)>
2012-06-02 14:35:50 +0400

    Added D3D9 objects example

---
+++ D3DX9/objects/main.cpp
@@ -0,0 +1,247 @@
+/*
+ *  Copyright (c) 2012 Evgeny Proydakov <lord.tiran@gmail.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+#include <cmath>
+#include <cassert>
+#include <iostream>
+
+#include <d3d9.h>
+#include <d3dx9mesh.h>
+
+
+#ifndef SAFE_RELEASE
+#define SAFE_RELEASE(p)      { if (p) { (p)->Release(); (p) = NULL; } }
+#endif
+
+#define VK_1 0x31
+#define VK_2 0x32
+#define VK_3 0x33
+
+enum objects 
+{ 
+    sphere, 
+    cube, 
+    torus 
+}; 
+
+static const FLOAT OBJECT_SIZE = 10;
+static const UINT OBJECT_PRECESSION = 100;
+
+static const FLOAT ROTATION_DELTA = 0.01f;
+
+//-----------------------------------------------------------------------------
+// Globals variables and definitions
+//-----------------------------------------------------------------------------
+LPDIRECT3D9       g_pD3D       = NULL;
+LPDIRECT3DDEVICE9 g_pd3dDevice = NULL;
+
+LPD3DXMESH g_pSphereMesh = NULL;
+LPD3DXMESH g_pCubeMesh   = NULL;
+LPD3DXMESH g_pTorusMesh  = NULL;
+
+static objects g_object = sphere;
+static FLOAT g_rotation = 0;
+
+//--------------------------------------------------------------------------------------
+// Forward declarations 
+//--------------------------------------------------------------------------------------
+bool init(HWND hWnd);
+void cleanup();
+void render();
+void resize(UINT width, UINT height);
+void keyboard(HWND hWnd, WPARAM key);
+LRESULT WINAPI msgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+
+INT WINAPI wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, INT)
+{
+    UNREFERENCED_PARAMETER(hInst);
+
+    CHAR appClass[] = "D3D Examples";
+
+    WNDCLASSEX wc =
+    {
+        sizeof(WNDCLASSEX), CS_CLASSDC, msgProc, 0L, 0L,
+        GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
+        appClass, NULL
+    };
+    RegisterClassEx(&wc);
+
+    HWND hWnd = CreateWindow(appClass, "D3D draw objects",
+                             WS_OVERLAPPEDWINDOW, 100, 100, 600, 600,
+                             NULL, NULL, wc.hInstance, NULL);
+
+    if(!init(hWnd)) {
+        return 0;
+    }
+
+    ShowWindow(hWnd, SW_SHOWDEFAULT);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+    while(GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    UnregisterClass(appClass, wc.hInstance);
+    return 0;
+}
+
+bool init(HWND hWnd)
+{
+    g_pD3D = Direct3DCreate9(D3D_SDK_VERSION);
+    if(!g_pD3D) {
+        return false;
+    }
+
+    D3DPRESENT_PARAMETERS d3dpp;
+    ZeroMemory(&d3dpp, sizeof(d3dpp));
+    d3dpp.Windowed = true;
+    d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+
+    g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
+        D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_pd3dDevice);
+
+    if(!g_pD3D) {
+        return false;
+    }
+
+    g_pd3dDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
+
+    D3DXCreateSphere(g_pd3dDevice, OBJECT_SIZE, OBJECT_PRECESSION, OBJECT_PRECESSION, &g_pSphereMesh, 0);
+    D3DXCreateBox(g_pd3dDevice, OBJECT_SIZE, OBJECT_SIZE, OBJECT_SIZE, &g_pCubeMesh, 0);
+    D3DXCreateTorus(g_pd3dDevice, OBJECT_SIZE / OBJECT_SIZE, OBJECT_SIZE, OBJECT_PRECESSION / (UINT) OBJECT_SIZE, OBJECT_PRECESSION, &g_pTorusMesh, 0);
+
+    return true;
+}
+
+void cleanup()
+{
+    SAFE_RELEASE(g_pSphereMesh);
+    SAFE_RELEASE(g_pCubeMesh);
+    SAFE_RELEASE(g_pTorusMesh);
+    SAFE_RELEASE(g_pd3dDevice);
+    SAFE_RELEASE(g_pD3D);
+}
+
+void render()
+{
+    if(!g_pd3dDevice) {
+        return;
+    }
+
+    D3DXVECTOR3 position(sin(g_rotation) * OBJECT_SIZE, cos(g_rotation) * OBJECT_SIZE, OBJECT_SIZE);
+    D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+    D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+    D3DXMATRIX V;
+    D3DXMatrixLookAtLH(&V, &position, &target, &up);
+    g_pd3dDevice->SetTransform(D3DTS_VIEW, &V);
+
+    g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(255, 0, 0), 1.0f, 0);
+
+    g_pd3dDevice->BeginScene();
+    {
+        switch(g_object) {
+            case sphere:
+                g_pSphereMesh->DrawSubset(0);
+                break;
+
+            case cube:
+                g_pCubeMesh->DrawSubset(0);
+                break;
+
+            case torus:
+                g_pTorusMesh->DrawSubset(0);
+                break;
+
+            default:
+                assert(!"Invalid object");
+                break;
+        }
+    }
+    g_pd3dDevice->EndScene();
+
+    g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+
+    g_rotation += ROTATION_DELTA;
+}
+
+void resize(UINT width, UINT height)
+{
+    D3DXMATRIX proj;
+    D3DXMatrixPerspectiveFovLH(
+        &proj,
+        D3DX_PI * 0.5f,
+        (FLOAT) width / (FLOAT) height,
+        0.1f,
+        100.0f);
+    g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &proj);
+}
+
+void keyboard(HWND hWnd, WPARAM key)
+{
+    switch(key) {
+    case VK_1:
+        g_object = sphere;
+        break;
+
+    case VK_2:
+        g_object = cube;
+        break;
+
+    case VK_3:
+        g_object = torus;
+        break;
+
+    case VK_ESCAPE:
+        DestroyWindow(hWnd);
+        break;
+    }
+}
+
+LRESULT WINAPI msgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg) {
+    case WM_DESTROY:
+        cleanup();
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+        render();
+        //ValidateRect(hWnd, NULL);
+        return 0;
+
+    case WM_SIZE:
+        RECT rect;
+        GetClientRect(hWnd, &rect);
+        resize(rect.right - rect.left, rect.bottom - rect.top);
+        break;
+
+    case WM_KEYDOWN:
+        keyboard(hWnd, wParam);
+        break;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
