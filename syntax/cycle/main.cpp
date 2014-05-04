4cf53dcad19693e6301ba53aa8c075a43d6e02be
e.proydakov <e.proydakov@gmail.com>
2014-05-04 22:12:10 +0400

    Added syntax cycle example

---
+++ syntax/cycle/main.cpp
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (c) 2014 Evgeny Proydakov <lord.tiran@gmail.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+#include <iostream>
+
+class helper
+{
+public:
+    static size_t size()
+    {
+        std::cout << "size()" << std::endl;
+        counter++;
+        return 10;
+    }
+
+    static void body()
+    {
+        std::cout << "body()" << std::endl;
+    }
+
+    static size_t get_counter()
+    {
+        return counter;
+    }
+
+private:
+    static size_t counter;
+};
+
+size_t helper::counter = 0;
+
+int main(int argc, char *argv[])
+{
+    (void) argc;
+    (void) argv;
+
+    std::cout << "before: " << helper::get_counter() << std::endl;
+
+    for(size_t i = 0; i < helper::size(); i++) {
+        helper::body();
+    }
+
+    std::cout << "after: " << helper::get_counter() << std::endl;
+
+    return 0;
+}
