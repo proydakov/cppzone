5055ac870976afed1597ff45c9ea2f796fb05602
Evgeny Proydakov <e.proydakov@gmail.com>
2014-05-29 20:26:08 +0400

    Added syntax template explicit specialization of function example

---
+++ syntax/template_explicit_specialization_of_function/main.cpp
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (c) 2014 Evgeny Proydakov <lord.tiran@gmail.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+#include <typeinfo>
+#include <iostream>
+
+class object
+{
+};
+
+template<class T>
+void debug_type()
+{
+    std::cout << "type: " << typeid(T).name() << std::endl;
+}
+
+template<>
+void debug_type<bool>()
+{
+    std::cout << "type: bool" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    (void) argc;
+    (void) argv;
+
+    debug_type<object>();
+    debug_type<bool>();
+
+    return 0;
+}
