91675d9d3f25307b711daedfb8f5ff941a33b6e9
Evgeny Proydakov <e.proydakov@rambler-co.ru>
2016-12-23 13:32:18 +0300

    Added system_bigalloc

---
+++ system/bigalloc/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    size_t s = 1l * 1024 * 1024 * 1024 / sizeof(int) * 4;
+    std::vector<int> blob( s, 0 );
+
+    size_t i = 0;
+    std::cout << "wait input..." << std::endl;
+    std::cin >> i;
+
+    return 0;
+}
