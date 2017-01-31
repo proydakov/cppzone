7ce3e54cdb307e2879f159eeeeaddf4d11a732e0
Evgeny Proydakov <e.proydakov@rambler-co.ru>
2017-01-31 20:14:50 +0300

    Update big file sort generator

--- task/big_file_sort/main_generate.cpp
+++ task/big_file_sort/main_generate.cpp
@@ -1,4 +1,7 @@
 #include <ctime>
+#include <mutex>
+#include <chrono>
+#include <cstdio>
 #include <string>
 #include <vector>
 #include <thread>
@@ -15,9 +18,31 @@ std::string pretty_size(double size)
     return std::to_string(size) + " GB";
 }
 
-void generate(const std::string& path, double size)
+template<class T>
+class cstorage
 {
-    std::ofstream output(path);
+public:
+    void push(T&& data)
+    {
+        std::lock_guard<std::mutex> guard(mutex_);
+        vector_.push_back(std::move(data));
+    }
+
+    void swap(std::vector<T>& vector)
+    {
+        std::lock_guard<std::mutex> guard(mutex_);
+        vector_.swap(vector);
+    }
+
+private:
+    std::mutex mutex_;
+    std::vector<T> vector_;
+};
+
+void producer(cstorage<std::vector<std::string>>& storage, double size)
+{
+    const size_t batch = 4096;
+    std::vector<std::string> data;
 
     double generated = 0;
     while(generated < size) {
@@ -30,7 +55,36 @@ void generate(const std::string& path, double size)
         std::string buffer(sstream.str());
         generated += buffer.size();
 
-        output << buffer;
+        data.push_back(buffer);
+        if(data.size() >= batch) {
+            storage.push(std::move(data));
+        }
+    }
+    storage.push(std::move(data));
+}
+
+void consumer(const std::string& file, cstorage<std::vector<std::string>>& storage, bool& done)
+{
+    std::ofstream output(file);
+
+    std::vector< std::vector<std::string> > buffer;
+    while(true) {
+        storage.swap(buffer);
+
+        if(buffer.empty()) {
+            if(done){
+                break;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+        else {
+            for(size_t i = 0; i < buffer.size(); i++) {
+                for(size_t j = 0; j < buffer[i].size(); j++) {
+                    output << buffer[i][j];
+                }
+            }
+            buffer.clear();
+        }
     }
 }
 
@@ -50,38 +104,34 @@ int main(int argc, char* argv[])
 
     srand (time(NULL));
 
-    std::vector<std::string> files;
     std::vector<std::thread> threads;
 
     const size_t hardware_concurrency = std::thread::hardware_concurrency();
     double segment_size = target / hardware_concurrency;
 
-    for(size_t i = 0; i < hardware_concurrency; i++) {
-        std::string blob_name(file + ".blob" +  std::to_string(i));
-        std::cout << "generate: " << blob_name
-                  << " size: "    << pretty_size(segment_size)
-                  << std::endl;
+    cstorage<std::vector<std::string>> storage;
 
-        threads.push_back(std::move(std::thread([blob_name, segment_size](){
-            generate(blob_name, segment_size);
+    for(size_t i = 0; i < hardware_concurrency; i++) {
+        threads.push_back(std::move(std::thread([&storage, segment_size](){
+            producer(storage, segment_size);
         })));
-        files.push_back(std::move(blob_name));
     }
+
+    bool done = false;
+
+    std::thread consumer_thread([&file, &storage, &done](){
+        consumer(file, storage, done);
+    });
 
     for(size_t i = 0; i < hardware_concurrency; i++) {
         threads[i].join();
     }
 
-    std::cout << "merge: " << file << " size: " << pretty_size(target) << std::endl;
+    done = true;
 
-    std::ofstream output(file);
-    for(size_t i = 0; i < files.size(); i++) {
-        std::ifstream input(files[i]);
-        std::string line;
-        while (std::getline(input, line)) {
-            output << line << "\n";
-        }
-    }
+    consumer_thread.join();
+
+    std::cout << "generated: " << file << " size: " << pretty_size(target) << std::endl;
 
     return 0;
 }
