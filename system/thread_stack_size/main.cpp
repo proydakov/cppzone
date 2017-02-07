e9806ae51f843b0f62913402962d65817bc44208
Evgeny Proydakov <e.proydakov@rambler-co.ru>
2017-02-07 15:07:50 +0300

    Added system_stack_size

---
+++ system/thread_stack_size/main.cpp
@@ -0,0 +1,24 @@
+#include <thread>
+#include <vector>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    const size_t size = 4096 - 1;
+    std::vector<std::thread> threads;
+    threads.reserve(size);
+
+    for(size_t i = 0; i < size; i++) {
+        std::thread thread([](){
+            std::this_thread::sleep_for (std::chrono::seconds(1 * 60 * 60));
+        });
+        thread.detach();
+        threads.push_back(std::move(thread));
+    }
+
+    int i = 0;
+    std::cout << "wait input..." << std::endl;
+    std::cin >> i;
+
+    return 0;
+}
