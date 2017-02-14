a79f1bd3e0a19f8910c330634915eaa2b7dbd66d
Evgeny Proydakov <e.proyakov@gmail.com>
2017-02-14 13:03:05 +0300

    Review

--- algorithms/vector_vs_list/main.cpp
+++ algorithms/vector_vs_list/main.cpp
@@ -32,54 +32,54 @@ int main()
     srand((unsigned int)time(NULL));
 
     std::vector<double> vector_iter_data;
-    std::vector<double> vector_index_data;
     std::list<double> list_data;
+    std::vector<double> vector_index_data;
 
     size_t data_size = 100000000;
     for(size_t i = 0; i < data_size; i++) {
-        double value = static_cast<double>(rand() % 1000) / 1000;
+        const double value = static_cast<double>(rand() % 1000) / 1000;
         vector_iter_data.push_back(value);
         vector_index_data.push_back(value);
         list_data.push_back(value);
     }
 
-	std::chrono::high_resolution_clock clock;
+    std::chrono::high_resolution_clock clock;
 
     ///////////////////////////////////////////////////////////////////////////
 
-	auto viterclock_start = clock.now();
+    auto viterclock_start = clock.now();
     double vector_iter_sum = 0;
     auto endVector = vector_iter_data.end();
     for(auto it = vector_iter_data.begin(); it != endVector; ++it) {
         vector_iter_sum += *it;
     }
-	auto viterclock_end = clock.now();
+    auto viterclock_end = clock.now();
 
     ///////////////////////////////////////////////////////////////////////////
 
-	auto vindexclock_start = clock.now();
+    auto vindexclock_start = clock.now();
     double vector_index_sum = 0;
     for(size_t i = 0; i < data_size; i++) {
         vector_index_sum += vector_index_data[i];
     }
-	auto vindexclock_end = clock.now();
+    auto vindexclock_end = clock.now();
 
     ///////////////////////////////////////////////////////////////////////////
 
-	auto lclock_start = clock.now();
+    auto lclock_start = clock.now();
     double list_sum = 0;
     auto endList = list_data.end();
     for(auto it = list_data.begin(); it != endList; ++it) {
         list_sum += *it;
     }
-	auto lclock_end = clock.now();
+    auto lclock_end = clock.now();
 
     ///////////////////////////////////////////////////////////////////////////
 
     std::cout << "sum elements num: " << data_size << std::endl;
-	std::cout << "vector iter  process time: " << std::chrono::duration_cast<std::chrono::microseconds>(viterclock_end - viterclock_start).count() << " us" << " result: " << vector_iter_sum << std::endl;
-	std::cout << "vector index process time: " << std::chrono::duration_cast<std::chrono::microseconds>(vindexclock_end - vindexclock_start).count() << " us" << " result: " << vector_index_sum << std::endl;
-	std::cout << "list process time:         " << std::chrono::duration_cast<std::chrono::microseconds>(lclock_end - lclock_start).count() << " us" << " result: " << list_sum << std::endl;
+    std::cout << "vector iter process time:  " << std::chrono::duration_cast<std::chrono::microseconds>(viterclock_end - viterclock_start).count() << " us" << " result: " << vector_iter_sum << std::endl;
+    std::cout << "vector index process time: " << std::chrono::duration_cast<std::chrono::microseconds>(vindexclock_end - vindexclock_start).count() << " us" << " result: " << vector_index_sum << std::endl;
+    std::cout << "list iter process time:    " << std::chrono::duration_cast<std::chrono::microseconds>(lclock_end - lclock_start).count() << " us" << " result: " << list_sum << std::endl;
 
     return 0;
 }
