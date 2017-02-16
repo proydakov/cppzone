eabf110bcabbbe1f72c7767eca7bcf1312cd05ea
Evgeny Proydakov <e.proyakov@gmail.com>
2017-02-16 12:35:27 +0300

    Added main_read. Test read data from disk speed.

---
+++ task/big_file_sort/main_read.cpp
@@ -0,0 +1,27 @@
+#include <array>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        std::cout << "usage: " << argv[0] << " path/to/file" << std::endl;
+        return 1;
+    }
+    std::string file(argv[1]);
+    std::ifstream input(file);
+
+    size_t counter = 0;
+    enum { buffer_size = 1024 * 1024 };
+    std::array<char, buffer_size> buffer;
+    while(!input.eof( )) {
+        input.read(buffer.data(), buffer_size);
+        const int gcount = input.gcount();
+        counter += std::count( buffer.data(), buffer.data() + gcount, '\n' );
+    }
+    std::cout << "file_size: " << counter << std::endl;
+
+    return 0;
+}
