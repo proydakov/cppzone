3a8848966c931b88dc515757bef6c30adbee470f
Evgeny Proydakov <e.proydakov@gmail.com>
2017-10-30 18:35:42 +0300

    Added stl.vec example

---
+++ stl/vec/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+
+void* operator new(std::size_t sz)
+{
+    std::cout << "malloc, size = " << sz << std::endl;
+    return std::malloc(sz);
+}
+
+void operator delete(void* ptr) noexcept
+{
+    std::cout << "free" << std::endl;
+    std::free(ptr);
+}
+
+int main()
+{
+    std::vector<int> vec;
+    std::cout << "created" << std::endl;
+
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+
+    std::cout << "size: " << vec.size() << std::endl;
+    std::cout << "capacity: " << vec.capacity() << std::endl;
+
+    return 0;
+}
