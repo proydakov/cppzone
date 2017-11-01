96362faba3f158fc95e9aa2a10e53bdf94fa06fe
Evgeny Proydakov <e.proydakov@gmail.com>
2017-11-01 13:07:53 +0300

    Added stl_str example

---
+++ stl/str/main.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+
+void* operator new(std::size_t sz)
+{
+    std::cout << "malloc, size = " << sz << std::endl;
+    return std::malloc(sz);
+}
+
+void operator delete(void* ptr) noexcept
+{
+    std::cout << "free" << std::endl;
+    std::free(ptr);
+}
+
+int main()
+{
+    std::string str;
+
+    for(int i = 0; i < 31; i++) {
+        str.push_back('0' + i);
+    }
+
+    std::cout << "size: " << str.size() << std::endl;
+    std::cout << "capacity: " << str.capacity() << std::endl;
+
+    return 0;
+}
