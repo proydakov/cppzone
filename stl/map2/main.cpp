3ef411cd5059f221234fc92742cb9d9a3e1ff380
Evgeny Proydakov <e.proydakov@gmail.com>
2017-11-08 22:46:20 +0300

    Added map2

---
+++ stl/map2/main.cpp
@@ -0,0 +1,33 @@
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+
+void* operator new(std::size_t sz)
+{
+    std::cout << "malloc, size = " << sz << std::endl;
+    return std::malloc(sz);
+}
+
+void operator delete(void* ptr) noexcept
+{
+    std::cout << "free" << std::endl;
+    std::free(ptr);
+}
+
+int safe_get(std::map<std::string, int>& map, const char* str)
+{
+    return map[str];
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+
+    std::map<std::string, int> map;
+    std::cout << "put" << std::endl;
+
+    safe_get(map, "big+str+in+heap+area+always");
+
+    return 0;
+}
