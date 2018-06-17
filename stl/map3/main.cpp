0cc195a71beb16b467c80e26a1d48c3d0ddae553
Evgeny Proydakov <e.proydakov@gmail.com>
2018-06-17 18:13:17 +0300

    Added map3

---
+++ stl/map3/main.cpp
@@ -0,0 +1,34 @@
+#include <map>
+#include <cstdint>
+#include <iostream>
+
+void* operator new(std::size_t sz)
+{
+    std::cout << "malloc, size = " << sz << std::endl;
+    return std::malloc(sz);
+}
+
+void operator delete(void* ptr)
+{
+    std::cout << "free" << std::endl;
+    std::free(ptr);
+}
+
+using map_t = std::map<std::int64_t, std::int64_t>;
+
+#ifdef _MSC_VER
+
+static_assert(sizeof(map_t::_Node) == 48, "Require fixed node size");
+
+#endif // _MSC_VER
+
+int main()
+{
+    std::cout << "value type: " << sizeof(map_t::value_type) << std::endl;
+
+    map_t map;
+    std::cout << "put" << std::endl;
+    map.insert(std::make_pair(0, 0));
+
+    return 0;
+}
