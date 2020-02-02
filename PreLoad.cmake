41aad7b045d9671cc41866f50ab0d796607d4807
Evgeny Proydakov <e.proydakov@gmail.com>
2020-02-02 21:03:44 +0300

    Try to fix windows build

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -8,7 +8,7 @@
 #  sudo apt-get install ninja-build
 
 find_program(NINJA_PATH ninja)
-if (NINJA_PATH)
+if (NOT WIN32 AND NINJA_PATH)
     set(CMAKE_GENERATOR "Ninja" CACHE INTERNAL "" FORCE)
 endif ()
 
