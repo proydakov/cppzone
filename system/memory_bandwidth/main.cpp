7d1f2f4f69a148e804f43a7654fa11674d3e73e3
Evgeny Proydakov <e.proydakov@gmail.com>
2020-02-05 00:45:51 +0300

    Updated memory bandwidth

--- system/memory_bandwidth/main.cpp
+++ system/memory_bandwidth/main.cpp
@@ -2,10 +2,11 @@
 #include <chrono>
 #include <vector>
 #include <numeric>
+#include <iomanip>
 #include <cstdint>
 #include <iostream>
 
-std::int64_t constexpr giga = 1024l * 1024l * 1024l;
+std::size_t constexpr giga = 1024ul * 1024ul * 1024ul;
 
 class experiment
 {
@@ -18,11 +19,10 @@ public:
 
     ~experiment()
     {
-        auto end = std::chrono::high_resolution_clock::now();
+        auto const end = std::chrono::high_resolution_clock::now();
 
         auto const delta = end - m_start;
-        std::cout << "fill size: " << double(m_bytes) / double(giga) << " Gb time: " << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count() << " milliseconds" << std::endl;
-        std::cout << "memory_bandwidth: " << double(m_bytes) / double(giga) / double(std::chrono::duration_cast<std::chrono::milliseconds>(delta).count() * 1000) << " GB/s" << std::endl;
+        std::cout << "memory_bandwidth: " << std::setprecision(3) << double(m_bytes) / double(giga) / double(std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()) * double(1000) << " GiB/s" << std::endl;
     }
 
 private:
@@ -42,7 +42,7 @@ int main(int argc, char* argv[])
     std::cout << "memory size: " << gb << " Gb" << std::endl;
 
     using vec_t = std::vector<std::int64_t>;
-    vec_t data;
+    vec_t origin;
 
     std::size_t const alloc_bytes = gb * giga;
     std::size_t const alloc_size = alloc_bytes / sizeof(vec_t::value_type);
@@ -50,15 +50,39 @@ int main(int argc, char* argv[])
     {
         std::cout << "\nallocating..." << std::endl;
         experiment e(alloc_bytes);
-        data.resize(alloc_size, 0);
+        origin.resize(alloc_size, 1);
     }
 
-    vec_t copy;
+    {
+        std::cout << "\nwriting..." << std::endl;
+        experiment e(alloc_bytes);
+        std::memset(origin.data(), 7, alloc_bytes);
+    }
+
+    {
+        std::cout << "\nreading..." << std::endl;
+        experiment e(alloc_bytes);
+        auto end_ptr = (volatile int64_t*)((char*)origin.data() + alloc_bytes);
+        for (auto ptr = (volatile int64_t*)(origin.data()); ptr < end_ptr; ptr++)
+        {
+            [[maybe_unused]] volatile int64_t val = *ptr;
+        }
+    }
+
+    vec_t copy(alloc_size, 2);
 
     {
         std::cout << "\ncopying..." << std::endl;
         experiment e(alloc_bytes);
-        copy = data;
+        std::memcpy(copy.data(), origin.data(), alloc_bytes);
+    }
+
+    int cmp = 0;
+
+    {
+        std::cout << "\ncomparing..." << std::endl;
+        experiment e(alloc_bytes);
+        cmp = std::memcmp(origin.data(), copy.data(), alloc_bytes);
     }
 
     int summ = 0;
@@ -66,7 +90,7 @@ int main(int argc, char* argv[])
     {
         std::cout << "\naccumulating..." << std::endl;
         experiment e(alloc_bytes);
-        summ = std::accumulate(data.begin(), data.end(), int{0});
+        summ = std::accumulate(origin.begin(), origin.end(), int{0});
     }
 
     {
@@ -75,5 +99,5 @@ int main(int argc, char* argv[])
         std::cin >> n;
     }
 
-    return summ;
+    return summ + cmp;
 }
