7c2994948d406135f86492dd4a494eec81c8fc2c
Evgeny Proydakov <e.proydakov@gmail.com>
2020-08-24 16:26:00 +0300

    Compilation fix for rdrand64

--- intrinsic/rdrand64/main.cpp
+++ intrinsic/rdrand64/main.cpp
@@ -10,7 +10,7 @@
 int main()
 {
     std::uint64_t val{0};
-    auto const res = _rdrand64_step(&val);
+    auto const res = _rdrand64_step((long long unsigned int*)&val);
     if (res)
     {
         std::cout << "Value: " << val << std::endl;
