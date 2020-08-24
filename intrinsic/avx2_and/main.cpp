2bdb3c6b8fbc82f9747b84c28a4f287a510d5f2e
Evgeny Proydakov <e.proydakov@gmail.com>
2020-08-24 18:01:48 +0300

    Added intrinsic_avx2_and example

--- intrinsic/avx2_and/main.cpp
+++ intrinsic/avx2_and/main.cpp
@@ -45,14 +45,14 @@ int main()
     avx2_int32vec_t input1;
     avx2_int32vec_t input2;
 
-    std::cout << "Sum 2 vector(int32_t)[" << avx2_int32vec_t::size() << "]\n";
+    std::cout << "Sum 2 vector(int32)[" << avx2_int32vec_t::size() << "]\n";
 
     read("input1", input1);
     read("input2", input2);
 
     auto avx1 = _mm256_load_si256(reinterpret_cast<craw_ptr>(&input1));
     auto avx2 = _mm256_load_si256(reinterpret_cast<craw_ptr>(&input2));
-    auto avxr = _mm256_add_epi32(avx1, avx2);
+    auto avxr = _mm256_and_si256(avx1, avx2);
 
     avx2_int32vec_t result;
     _mm256_store_si256(reinterpret_cast<raw_ptr>(&result), avxr);
