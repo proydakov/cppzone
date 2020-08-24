38c0bee4a0a424a6fbf37a2a7099e218e453b6fd
Evgeny Proydakov <e.proydakov@gmail.com>
2020-08-24 17:57:17 +0300

    Added avx512_and example

--- intrinsic/avx512_and/main.cpp
+++ intrinsic/avx512_and/main.cpp
@@ -45,14 +45,14 @@ int main()
     avx512_int32vec_t input1;
     avx512_int32vec_t input2;
 
-    std::cout << "Sum 2 vector(int32_t)[" << avx512_int32vec_t::size() << "]\n";
+    std::cout << "And 2 vector(int32_t)[" << avx512_int32vec_t::size() << "]\n";
 
     read("input1", input1);
     read("input2", input2);
 
     auto avx1 = _mm512_load_si512(reinterpret_cast<craw_ptr>(&input1));
     auto avx2 = _mm512_load_si512(reinterpret_cast<craw_ptr>(&input2));
-    auto avxr = _mm512_add_epi32(avx1, avx2);
+    auto avxr = _mm512_and_si512(avx1, avx2);
 
     avx512_int32vec_t result;
     _mm512_store_si512(reinterpret_cast<raw_ptr>(&result), avxr);
