913cb025a9a89682f3b8f67e7c648b3c2a6a76bf
Evgeny Proydakov <e.proydakov@gmail.com>
2020-08-25 18:53:48 +0300

    Fixed avx512 linux gcc build

--- intrinsic/avx512_negative/main.cpp
+++ intrinsic/avx512_negative/main.cpp
@@ -14,7 +14,7 @@ int main()
 
     read("input1", input1);
 
-    auto avx1 = _mm512_stream_load_si512(reinterpret_cast<craw_ptr>(&input1));
+    auto avx1 = _mm512_load_si512(reinterpret_cast<craw_ptr>(&input1));
     auto avx_zero = _mm512_setzero_si512();
     auto avx_mask = _mm512_int2mask(0b1111111111111111);
     auto avxr = _mm512_mask_sub_epi32(avx1, avx_mask, avx_zero, avx1);
