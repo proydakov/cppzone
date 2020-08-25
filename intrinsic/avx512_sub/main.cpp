5093027a0086d3c52cfa580b0ed00fe8b8ba0053
Evgeny Proydakov <e.proydakov@gmail.com>
2020-08-25 18:44:16 +0300

    Added intrinsic avx512 sub example

--- intrinsic/avx512_sub/main.cpp
+++ intrinsic/avx512_sub/main.cpp
@@ -11,14 +11,14 @@ int main()
     avx512_int32vec_t input1;
     avx512_int32vec_t input2;
 
-    std::cout << "Add 2 vector(int32_t)[" << avx512_int32vec_t::size() << "]\n";
+    std::cout << "Sub 2 vector(int32_t)[" << avx512_int32vec_t::size() << "]\n";
 
     read("input1", input1);
     read("input2", input2);
 
     auto avx1 = _mm512_load_si512(reinterpret_cast<craw_ptr>(&input1));
     auto avx2 = _mm512_load_si512(reinterpret_cast<craw_ptr>(&input2));
-    auto avxr = _mm512_add_epi32(avx1, avx2);
+    auto avxr = _mm512_sub_epi32(avx1, avx2);
 
     avx512_int32vec_t result;
     _mm512_store_si512(reinterpret_cast<raw_ptr>(&result), avxr);
