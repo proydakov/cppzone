c13f1a45bedd08e506f552d4e8a9a01cc2c9d7ef
Evgeny Proydakov <e.proydakov@gmail.com>
2020-08-25 18:23:07 +0300

    Added intrinsic avx2 sub example

--- intrinsic/avx2_sub/main.cpp
+++ intrinsic/avx2_sub/main.cpp
@@ -11,14 +11,14 @@ int main()
     avx2_int32vec_t input1;
     avx2_int32vec_t input2;
 
-    std::cout << "Add 2 vector(int32_t)[" << avx2_int32vec_t::size() << "]\n";
+    std::cout << "Sub 2 vector(int32_t)[" << avx2_int32vec_t::size() << "]\n";
 
     read("input1", input1);
     read("input2", input2);
 
     auto avx1 = _mm256_load_si256(reinterpret_cast<craw_ptr>(&input1));
     auto avx2 = _mm256_load_si256(reinterpret_cast<craw_ptr>(&input2));
-    auto avxr = _mm256_add_epi32(avx1, avx2);
+    auto avxr = _mm256_sub_epi32(avx1, avx2);
 
     avx2_int32vec_t result;
     _mm256_store_si256(reinterpret_cast<raw_ptr>(&result), avxr);
