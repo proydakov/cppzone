88c7e2b77fb400a45bb8084a373adacabec716c2
Evgeny Proydakov <e.proydakov@gmail.com>
2020-11-16 17:15:27 +0300

    Cleanup cmake in alg/mt folders.

--- cmake/platform/shared/FindSDL2.cmake
+++ cmake/platform/shared/FindSDL2.cmake
@@ -56,19 +56,10 @@ IF(SDL2_LIBRARY_TEMP)
     ENDIF(SDL2_MAIN_LIBRARY)
   ENDIF(NOT SDL2_BUILDING_LIBRARY)
 
-  # For OS X, SDL2 uses Cocoa as a backend so it must link to Cocoa.
-  # CMake doesn't display the -framework Cocoa string in the UI even
-  # though it actually is there if I modify a pre-used variable.
-  # I think it has something to do with the CACHE STRING.
-  # So I use a temporary variable until the end so I can set the
-  # "real" variable in one-shot.
   IF(APPLE)
     SET(SDL2_LIBRARY_TEMP ${SDL2_LIBRARY_TEMP} "-framework Cocoa")
   ENDIF(APPLE)
 
-  # For threads, as mentioned Apple doesn't need this.
-  # In fact, there seems to be a problem if I used the Threads package
-  # and try using this line, so I'm just skipping it entirely for OS X.
   IF(NOT APPLE)
     SET(SDL2_LIBRARY_TEMP ${SDL2_LIBRARY_TEMP} ${CMAKE_THREAD_LIBS_INIT})
   ENDIF(NOT APPLE)
