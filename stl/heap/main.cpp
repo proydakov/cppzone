358ae9eb620456939ab8638119ba416a5de8bcc0
Evgeny Proydakov <e.proydakov@gmail.com>
2021-02-21 01:43:25 +0300

    Updated STL heap example

--- stl/heap/main.cpp
+++ stl/heap/main.cpp
@@ -76,11 +76,40 @@ void heap_2()
     std::cout << v << std::endl;
 }
 
+void heap_3()
+{
+    std::vector<int> v { 3, 1, 4, 1, 5, 9 };
+ 
+    std::make_heap(v.begin(), v.end());
+ 
+    std::cout << "v: ";
+    for (auto i : v) std::cout << i << ' ';
+    std::cout << '\n';
+ 
+    v.push_back(6);
+ 
+    std::cout << "before push_heap: ";
+    for (auto i : v) std::cout << i << ' ';
+    std::cout << '\n';
+ 
+    std::push_heap(v.begin(), v.end());
+ 
+    std::cout << "after push_heap:  ";
+    for (auto i : v) std::cout << i << ' ';
+    std::cout << '\n';
+}
+
 int main()
 {
     heap_1();
 
+    std::cout << "\n";
+
     heap_2();
+
+    std::cout << "\n";
+
+    heap_3();
 
     return 0;
 }
